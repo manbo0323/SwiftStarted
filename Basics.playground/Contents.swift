f526c8a8313469547edabb2967a9274d6792bdb0
manboyu <myu@hkesports.com>
2016-10-04 15:15:11 +0800

    Swift Initial commit

---
+++ Basics.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// 使用 let 宣告常數 name
+let name = "Joe"
+
+// 使用 var 宣告變數 price
+var price = 300
+
+// 接著就可以使用這個宣告過的常數 用來印出名字
+print(name)
+
+let score = 100
+
+
+let myFullName = "Kevin Chen"
+
+//var 123myName = "Jess" // 不能以數字開頭 所以這行會報錯誤
+var myClass55 = "101" // 在其他部分使用數字則是沒問題
+print(myClass55)
+myClass55 = "102"
+print(myClass55)
